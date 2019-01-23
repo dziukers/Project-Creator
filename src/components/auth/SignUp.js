4fcc67b889cbfce5c89973be6c43db3f8b84c42e
Krystian Dziuk <dziukers@gmail.com>
2019-01-23 23:07:07 +0100

    added signing up, redirecting

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { signUp } from '../../store/actions/authActions';
 
 class SignUp extends Component {
     state = {
@@ -15,9 +18,11 @@ class SignUp extends Component {
     }
     handleSubmit = (e) => {
       e.preventDefault();
-      console.log(this.state);
+      this.props.signUp(this.state)
     }
   render() {
+    const { auth, authError } = this.props;
+    if (auth.uid) return <Redirect to='/' />
     return (
       <div className='container'>
         <form onSubmit={this.handleSubmit} className=''>
@@ -39,7 +44,10 @@ class SignUp extends Component {
               <input type="password" id='password' onChange={this.handleChange} />
             </div>
             <div className="input-field">
-              <button className="btn pink lighten-1 z-depth-0">Login</button>
+              <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+              <div className="red-text center">
+                {authError ? <p>{ authError }</p> : null }
+              </div>
             </div>
         </form>
       </div>
@@ -47,4 +55,17 @@ class SignUp extends Component {
   }
 }
 
-export default SignUp;
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth,
+    authError: state.auth.authError
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUp: (newUser) => dispatch(signUp(newUser))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,22 +4,22 @@ import SignInLinks from './SignInLinks';
 import SignOutLinks from './SignOutLinks';
 import { connect } from 'react-redux';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const {auth} = props;
+    const links = auth.uid ? <SignInLinks/> : <SignOutLinks/>;
     return (
         <nav className='nav-wrapper transparent'>
             <div className='container'>
                 <Link to='/' className='brand-logo'>Project Creator</Link>
-                <SignInLinks/>
-                <SignOutLinks/>
+                {links}
             </div>
         </nav>
     )
 }
 
 const mapStateToProps = state => {
-    console.log(state)
     return {
-
+        auth: state.firebase.auth
     }
 }
 
